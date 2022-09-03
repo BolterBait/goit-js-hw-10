fa5e1ba676ee1dc91d133df84fc4b5a4c525f950
Oleksandr Svyatobog <osvyatobog@gmail.com>
2022-09-03 21:04:53 +0300

    Task10-pending

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
+import debounce from 'lodash.debounce'
+import  Notiflix from 'notiflix';
 import './css/styles.css';
+import API from './fetchCountries';
 
-const DEBOUNCE_DELAY = 300;
+
+const inputField = document.querySelector('#search-box');
+
+inputField.addEventListener('input', debounce(onSearch, 300))
+function onSearch(evt) {
+const searchQuery = inputField.value.toLowerCase().trim();
+
+if (searchQuery !== ' ' ) {
+    API.fetchCountries(searchQuery)
+    .then((response) => {
+        if (!response.ok || response.status === 404) {
+            throw new Error(response.status);            
+        }return response.json();
+    }).then((data) => {
+        const countryListLength = data.length;
+        if (countryListLength > 10) { Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+        return;
+    }if (countryListLength >= 2 && countryListLength <= 10) { 
+        renderCountryUl(data);
+        
+    }
+    });
+}
+
+renderCountryUl(arr)
+  
+//     API.fetchCountries(searchQuery)
+// .then(country => {console.log(country);})
+// .catch(error => {console.log(error);})
+// .then(response=> {return response.json();}
+
+
+
+}
+
