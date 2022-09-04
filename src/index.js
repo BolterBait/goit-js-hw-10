6ae5cc686297f88044da15b26b3e4d3169ab61cc
Oleksandr Svyatobog <osvyatobog@gmail.com>
2022-09-04 13:33:05 +0300

    Task10-pending

--- src/index.js
+++ src/index.js
@@ -5,11 +5,25 @@ import API from './fetchCountries';
 
 
 const inputField = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryDiv = document.querySelector('.country-info');
+
+function clearCountryListMarkup() {
+    countryList.innerHTML=' ';
+    }
+
+function clearCountryDivMarkup() {
+    countryDiv.innerHTML = ' ';
+    }
 
 inputField.addEventListener('input', debounce(onSearch, 300))
-function onSearch(evt) {
+function onSearch() {
 const searchQuery = inputField.value.toLowerCase().trim();
-
+if (!searchQuery) {
+    clearCountryDivMarkup();
+    clearCountryListMarkup();
+    return;    
+}
 if (searchQuery !== ' ' ) {
     API.fetchCountries(searchQuery)
     .then((response) => {
@@ -21,20 +35,41 @@ if (searchQuery !== ' ' ) {
         if (countryListLength > 10) { Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
         return;
     }if (countryListLength >= 2 && countryListLength <= 10) { 
+        clearCountryDivMarkup()
         renderCountryUl(data);
-        
-    }
-    });
+    }if (countryListLength === 1) {
+        clearCountryListMarkup();
+        renderCountryDiv(data)
+        }
+    }).catch(error => {Notiflix.Notify.failure("Oops, there is no country with that name");
+    clearCountryDivMarkup();
+    clearCountryListMarkup();
+});
+}
 }
 
-renderCountryUl(arr)
-  
-//     API.fetchCountries(searchQuery)
-// .then(country => {console.log(country);})
-// .catch(error => {console.log(error);})
-// .then(response=> {return response.json();}
-
-
-
+function renderCountryUl(arr) {
+    countryList.innerHTML = arr.map(({ name, flags }) =>     
+`<li class="contry-item"><img class="flag-img" src="${flags.svg}" 
+alt="${name.official}flag" 
+width="60" 
+height="40"></>
+<span class="name_official">${name.official}
+</span>
+</li>`).join('')
 }
 
+function renderCountryDiv(arr) {
+    countryDiv.innerHTML = arr.map(({ flags, name, capital, population, languages }) =>
+    `<img class="flag-img" src="${flags.svg}" 
+    alt="${name.official}flag" 
+    width="100" 
+    height="60"></>
+    <h1>${name.official}</hi>
+    <p class="info-item">
+    <span>Capital: </span>${capital}</p>
+    <p class="info-item">
+    <span>Population: </span>${population}</p>
+    <p class="info-item">
+    <span>Languages: </span>${Object.values(languages)}</p>`).join('')    
+}
